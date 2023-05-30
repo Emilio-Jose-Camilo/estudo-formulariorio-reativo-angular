1d46980d938be0d70dcff6d7ee040f6a38c14430
Emiliojcm <121568964+Emiliojcm@users.noreply.github.com>
2023-05-30 16:16:24 -0300

    Formulário reatvo

---
+++ src/app/reactive-form-validation/reactive-form-validation.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-form-validation',
+  templateUrl: './reactive-form-validation.component.html',
+  styleUrls: ['./reactive-form-validation.component.scss']
+})
+export class ReactiveFormValidationComponent implements OnInit{
+
+  clientForm = this.fb.group({
+    firstName: ['', [Validators.required, Validators.minLength(3)]],
+    lastName: ['', [Validators.required, Validators.minLength(3)]],
+    birth: [new Date(), [Validators.required, Validators.minLength(1), Validators.maxLength(150)]],
+    age: [0, [Validators.required, Validators.minLength(1)]],
+    email: ['', [Validators.required, Validators.email]],
+    street: ['', [Validators.required]],
+    city: ['', [Validators.required]],
+    state: ['', [Validators.required]],
+    phone1: ['', [Validators.required]],
+    phone2: ['', [Validators.required]],
+  });
+
+
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+
+  }
+
+  onSubmit() {}
+}
